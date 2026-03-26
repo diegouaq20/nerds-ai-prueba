25c8c18a4f313b25a7ad65fb0abf698a7564af1f
Diego Martínez Tabarez <dmartinez145@alumnos.uaq.mx>
2026-03-26 17:28:17 -0600

    fix: Rutas de redireccion a swagger

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use('/text', textRoutes);
 
 app.get('/', (req, res) => {
   res.redirect('/docs');
 });
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/text', textRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
